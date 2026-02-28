1f4cabbd9ea7ebe30b31fa40f1dc222d2a5eb419
Bielsampaio25 <bielsampaio25@gmail.com>
2026-02-28 15:24:11 -0300

    organização do código CSS nas pastas: reset, colors, style e login; atualização dos caminhos CSS nos arquivos HTML e customização do formulário de login.

--- src/main/java/connection/ConnectionFactory.java
+++ src/main/java/connection/ConnectionFactory.java
@@ -18,7 +18,7 @@ public class ConnectionFactory {
         private static final String URL =
             "jdbc:mysql://localhost:3306/estoque_db?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
         private static final String USER = "root";
-        private static final String PASSWORD = "cimatec";
+        private static final String PASSWORD = "25042007";
     //Métodos
         
         public static Connection getConnection(){
