839ad969e8c6168724ab290f57a630349df5fd22
GABRIEL DE ALMEIDA SAMPAIO <232.004662@universidadesenaicimatec.edu.br>
2026-04-11 12:11:18 -0300

    atualização das configurações para acesso ao DB

--- src/main/java/connection/ConnectionFactory.java
+++ src/main/java/connection/ConnectionFactory.java
@@ -13,22 +13,32 @@ import java.sql.DriverManager;
  * @author Usuario
  */
 public class ConnectionFactory {
-    //Dados da conexão
+        private static final String URL = ("DB_URL");
+        private static final String USER = ("DB_USER");
+        private static final String PASSWORD = ("DB_PASS");
         private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
-        private static final String URL =
-            "jdbc:mysql://db:3306/estoque_db?estoque_dbSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
-        private static final String USER = "root";
-        private static final String PASSWORD = "cimatec";
-    //Métodos
         
         public static Connection getConnection(){
             Connection con = null;
             
             try {
+                if(URL == null || USER == null || PASSWORD == null){
+                    System.out.println("variavel de ambiente com problemas");
+                    return null;
+                }
+
                 Class.forName(DRIVER);
+
                 con = DriverManager.getConnection(URL, USER, PASSWORD);
                 System.out.println("Banco de dados conectado.");
-            } catch (Exception e) {
+
+            }catch(ClassNotFoundException e){
+                System.out.println("Erro no JDBC");
+                e.printStackTrace();
+            }catch(SQLException e){
+                System.out.println("Erro no SQL");
+                e.printStackTrace();
+            }catch (Exception e) {
                 System.out.println("Banco de dados não conectado");
                 e.printStackTrace();
             }
