587cd7049bcfa3af92bb65f281a7f5f189e6a0ea
GABRIEL DE ALMEIDA SAMPAIO <232.004662@universidadesenaicimatec.edu.br>
2026-04-11 09:40:15 -0300

    implementação de segurança no código

--- db/init.sql
+++ db/init.sql
@@ -1,42 +1,64 @@
 USE estoque_db;
 
-CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    
-    username VARCHAR(50) NOT NULL,
-    psw VARCHAR(255) NOT NULL,
-    
-    nameFirst VARCHAR(100),
-    sobrenome VARCHAR(100),
-    
-    matricula VARCHAR(50),
-    cpf VARCHAR(14),
-    
-    sexo VARCHAR(10),
-    dtaNascimento DATE,
-    
-    email VARCHAR(150),
-    telefone VARCHAR(20),
-    
-    funcao VARCHAR(100),
-    
-    cep VARCHAR(10),
-    endereco VARCHAR(150),
-    numero VARCHAR(10),
-    bairro VARCHAR(100),
-    cidade VARCHAR(100),
-    estado VARCHAR(50),
-    complemento VARCHAR(150),
-    
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+CREATE TABLE users(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(100) NOT NULL,
+    psw VARCHAR(100) NOT NULL,
+    nome VARCHAR(50) NOT NULL,
+    sobrenome VARCHAR(50) NOT NULL,
+    matricula VARCHAR(100) NOT NULL UNIQUE,
+    cpf VARCHAR(14) NOT NULL UNIQUE,
+    sexo VARCHAR(20) NOT NULL,
+    data_nasc DATE NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    telefone VARCHAR(100) NOT NULL,
+    funcao VARCHAR(50) NOT NULL,
+    cep VARCHAR(20) NOT NULL,
+    estado VARCHAR(50) NOT NULL,
+    cidade VARCHAR(50) NOT NULL,
+    endereco VARCHAR(100) NOT NULL,
+    bairro VARCHAR(50) NOT NULL,
+    numero VARCHAR(50) NOT NULL,
+    complemento VARCHAR(50)
 );
 
-INSERT INTO users (
-    username, psw, nameFirst, sobrenome, matricula, cpf,
-    sexo, dtaNascimento, email, telefone, funcao,
-    cep, endereco, numero, bairro, cidade, estado, complemento
-) VALUES (
-    'admin', '1234', 'Admin', 'Sistema', '0001', '000.000.000-00',
-    'Masculino', '1990-01-01', 'admin@email.com', '71999999999', 'Administrador',
-    '40000-000', 'Rua Exemplo', '123', 'Centro', 'Salvador', 'BA', 'N/A'
+INSERT INTO users(
+    username, 
+    psw,
+    nome, 
+    sobrenome, 
+    matricula, 
+    cpf, 
+    sexo,
+    data_nasc,
+    email, 
+    telefone, 
+    funcao,
+    cep, 
+    estado,
+    cidade,
+    endereco, 
+    bairro, 
+    numero, 
+    complemento
+)
+VALUES(
+    'admin', 
+    '1234', 
+    'Roberto', 
+    'Santos Ferreira', 
+    '00011234', 
+    '954.432.600-10', 
+    'Masculino',
+    '1990-05-15',
+    'roberto_ferreira32@gmail.com', 
+    '(71) 98876-5432', 
+    'Analista Administrativo',
+     '41830-020', 
+    'BA',
+    'Salvador',
+    'Avenida Tancredo Neves', 
+    'Caminho das Árvores', 
+    '1500', 
+    'Sala 1203, Torre Norte'
 );
